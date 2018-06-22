6a3a41627f33737cdc05e90046e593321fc0ab4c
sepehrabdoli <sepehrskater@yahoo.com>
2018-06-22 14:39:17 +0200

    php klaar

--- mathbirb-Inloggen.php
+++ mathbirb-Inloggen.php
@@ -1,3 +1,67 @@
+<?php
+
+$vnaamerr = $pswerr = "";
+$vnaam = $psw =  "";
+if ($_SERVER['REQUEST_METHOD'] =='POST'){
+  if(empty($_POST['vnaam']))
+  {
+    $vnaamerr = "voornaam is een verplicht veld";
+  }
+  else{
+    $vnaam = $_POST["vnaam"];
+  }
+
+  if(empty($_POST['psw']))
+  {
+    $pswerr = "telefoonummer is een verplicht veld";
+  }
+  else{
+    $psw = $_POST["psw"];
+  }
+}
+
+if($vnaamerr == "" && $pswerr == ""){
+	require('mathbirb-connect.php');
+  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $submitusername = $_POST['uname'];
+    $submitpassword = ($_POST['psw']);
+
+	function test_input($data){
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+	$vnaam = test_input($vnaam);
+  $psw = test_input($psw);
+	$vnaam = mysqli_real_escape_string($conn,$vnaam);
+  $psw = mysqli_real_escape_string($conn,$psw);
+
+  $query = "SELECT * FROM login WHERE username = '$submitusername' AND password = '$submitpassword'";
+  $result = mysqli_query($conn,$query);
+
+  if(!$result){
+    die("Query niet gelukt");
+  }
+  $aantal = mysqli_num_rows($result);
+  if($aantal == 1){
+    session_start();
+    $user = mysqli_fetch_row($result);
+    $_SESSION['ingelogd'] = "ja";
+    $_SESSION['username'] = $user[1];
+    header('Location: success.php');
+  }
+  else {
+    header("Location: mathbirb-inloggen.php");
+  }
+}
+  else{
+  header("Location: mathbirb-inloggen.php");
+}
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +75,6 @@
   <link href="css/inloggen.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-
-
   <!--Navbar--->
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
@@ -37,12 +99,15 @@
   <form action="mathbirb-inloggen.php" method="POST">
     <div class="container">
       <label for="uname"><b style="color:#33ff33;">Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" value="<?php echo $vnaam;?>"/>/>
+      <input type="text" placeholder="Enter Username" name="uname" value="<?php echo $uname;?>"/>
+      <span class="left"> <?php echo $vnaamerr;?></span><br>
 
       <label for="psw"><b style="color:#33ff33;">Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <input type="password" placeholder="Enter Password" name="psw" value="<?php echo $psw;?>"/>>
+      <span class="left"> <?php echo $pswerr;?></span><br>
 
-      <button style="background-color:#33ff33;" type="submit">Login</button>
+
+      <button style="background-color:#33ff33;" type="submit" value="login">Login</button>
     </div>
   </form>
 </div>
